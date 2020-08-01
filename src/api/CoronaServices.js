181a2e002d0f9161a3239306d9685fa975d8b70b
unknown <angga.q.suwito@gmail.com>
2020-08-01 12:40:09 +0800

    [UPDATE]add base URL

--- src/api/CoronaServices.js
+++ src/api/CoronaServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:3000/"
+const baseURL = "http://api.kawalcorona.com/"
 
 const getIndonesiaCoronaData = async () => {
     let datas = await axios.get(`${baseURL}indonesia/`)
